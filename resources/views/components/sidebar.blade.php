ac10367b9d2792559b87fd71e96ab70b23b72dc2
sandeepstha46 <sandeepstha64@gmail.com>
2022-12-24 21:15:59 +0545

    [Add] Active Class

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -7,25 +7,25 @@
          <div class="sidebar__content">
              <p class="content__title">Menu</p>
              <ul class="list-unstyled sidebar__content--list">
-                 <li class="menu menu-active">
+                 <li class="menu {{ Route::is('home') ? 'menu-active' : '' }}">
                      <a href="/">
                          <i class="bi bi-house-door"></i>
                          <span>Home</span>
                      </a>
                  </li>
-                 <li class="menu">
+                 <li class="menu {{ Route::is('discovery') ? 'menu-active' : '' }}">
                      <a href="{{ route('discovery') }}">
                          <i class="bi bi-folder"></i>
                          <span>Discovery</span>
                      </a>
                  </li>
-                 <li class="menu">
+                 <li class="menu {{ Route::is('trending') ? 'menu-active' : '' }}">
                      <a href="{{ route('trending') }}">
                          <i class="bi bi-fire"></i>
                          <span>Trending</span>
                      </a>
                  </li>
-                 <li class="menu">
+                 <li class="menu {{ Route::is('top-rated') ? 'menu-active' : '' }}">
                      <a href="{{ route('top-rated') }}">
                          <i class="bi bi-hand-thumbs-up"></i>
                          <span>Top Rated</span>
@@ -38,19 +38,19 @@
          </div>
          <div class="sidebar__content sidebar__content--second">
              <ul class="list-unstyled sidebar__content--list">
-                 <li>
+                 <li class="menu {{ Route::is('movies') ? 'menu-active' : '' }}">
                      <a href="{{ route('movies') }}">
                          <i class="bi bi-camera-reels"></i>
                          <span>Movies</span>
                      </a>
                  </li>
-                 <li>
+                 <li class="menu {{ Route::is('series') ? 'menu-active' : '' }}">
                      <a href="{{ route('series') }}">
                          <i class="bi bi-fast-forward-circle"></i>
                          <span>Series</span>
                      </a>
                  </li>
-                 <li>
+                 <li class="menu {{ Route::is('settings') ? 'menu-active' : '' }}">
                      <a href="{{ route('settings') }}">
                          <i class="bi bi-gear"></i>
                          <span>Settings</span>
